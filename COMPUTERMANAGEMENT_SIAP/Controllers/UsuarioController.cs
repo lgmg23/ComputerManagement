2f71dfcc4448fd2257c293154500dc2884299baa
Luis Gerardo Madrigal <luisgerardo_mg@hotmail.com>
2018-11-07 20:12:09 -0600

    cambios en db para llenar de usuarios...

--- COMPUTERMANAGEMENT_SIAP/Controllers/UsuarioController.cs
+++ COMPUTERMANAGEMENT_SIAP/Controllers/UsuarioController.cs
@@ -27,7 +27,7 @@ namespace COMPUTERMANAGEMENT_SIAP.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult UsuarioBusqueda(UsuarioBusquedaModel usuarioModel)
+        public ActionResult UsuarioBusqueda(UsuarioModel datad)
         {
             var config = new MapperConfiguration(cfg =>
             {
@@ -35,13 +35,13 @@ namespace COMPUTERMANAGEMENT_SIAP.Controllers
                 cfg.CreateMap<UsuarioModel, t_Usuario>();
 
             });
-            IMapper iMapper = config.CreateMapper();
-            var usuario = usuarioModel.Usuario;
-            DirectoryEntry adCN = new DirectoryEntry("LDAP://GAP.NET", "soportea.ext2", "Gerard1995#");
-            DirectorySearcher busqueda = new DirectorySearcher(adCN);            
-            busqueda.Filter = "(&(objectCategory=person)(objectClass=user)(|(displayName=*" + usuario + "*)(givenName=*" + usuario + "*)(samaccountname=*" + usuario + "*)(sn=*" + usuario + "*)))";
-            var results = busqueda.FindAll();
             List<UsuarioModel> modelList = new List<UsuarioModel>();
+            IMapper iMapper = config.CreateMapper();
+            //var usuario = usuarioModel.Usuario;
+            DirectoryEntry adCN = new DirectoryEntry("LDAP://GAP.NET", "soportea.ext2", "Gerard1995#");
+            DirectorySearcher busqueda = new DirectorySearcher(adCN);
+            busqueda.Filter = "(&(objectCategory=person)(objectClass=user))";
+            var results = busqueda.FindAll();
             if (results != null)
             {
                 foreach (SearchResult result in results)
@@ -56,9 +56,9 @@ namespace COMPUTERMANAGEMENT_SIAP.Controllers
                         {
                             model.Usuario = result.Properties["samaccountname"][0].ToString();
                             model.NombreCompleto = result.Properties["cn"][0].ToString();
-                            model.Detalle = result.Properties["description"][0].ToString();
-                            if (result.Properties["telephonenumber"].Count > 0 && result.Properties["mail"].Count > 0)
+                            if (result.Properties["telephonenumber"].Count > 0 && result.Properties["mail"].Count > 0 && result.Properties["description"].Count > 0 )
                             {
+                                model.Detalle = result.Properties["description"][0].ToString();
                                 model.Correo = result.Properties["mail"][0].ToString();
                                 int telefonoA = 0;
                                 Int32.TryParse(result.Properties["telephonenumber"][0].ToString(), out telefonoA);
@@ -72,8 +72,44 @@ namespace COMPUTERMANAGEMENT_SIAP.Controllers
                         }
                     }
                 }
-            }            
-            return View("Usuarios", modelList);
+            }
+                    //busqueda.Filter = "(&(objectCategory=person)(objectClass=user)(|(displayName=*" + usuario + "*)(givenName=*" + usuario + "*)(samaccountname=*" + usuario + "*)(sn=*" + usuario + "*)))";
+                    /*
+                    var results = busqueda.FindAll();
+                    List<UsuarioModel> modelList = new List<UsuarioModel>();
+                    if (results != null)
+                    {
+                        foreach (SearchResult result in results)
+                        {
+                            if (result.Properties["distinguishedname"] != null && result.Properties["distinguishedname"].Count > 0)
+                            {
+                                UsuarioModel model = new UsuarioModel();
+                                COMPUTERMANAGEMENT_TestEntities _context = new COMPUTERMANAGEMENT_TestEntities();
+                                string user = result.Properties["samaccountname"][0].ToString();
+                                var checkIfExists = _context.t_Usuario.FirstOrDefault(x => x.Usuario == user);
+                                if (checkIfExists == null)
+                                {
+                                    model.Usuario = result.Properties["samaccountname"][0].ToString();
+                                    model.NombreCompleto = result.Properties["cn"][0].ToString();
+                                    model.Detalle = result.Properties["description"][0].ToString();
+                                    if (result.Properties["telephonenumber"].Count > 0 && result.Properties["mail"].Count > 0)
+                                    {
+                                        model.Correo = result.Properties["mail"][0].ToString();
+                                        int telefonoA = 0;
+                                        Int32.TryParse(result.Properties["telephonenumber"][0].ToString(), out telefonoA);
+                                        model.Telefono = telefonoA;
+                                    }
+                                    var source = model;
+                                    modelList.Add(source);
+                                    var destination = iMapper.Map<UsuarioModel, t_Usuario>(source);
+                                    _context.t_Usuario.Add(destination);
+                                    _context.SaveChanges();
+                                }
+                            }
+                        }
+                    } 
+                    */
+                    return View("Usuarios", modelList);
         }
         [HttpGet]
         public ActionResult UsuarioDetalle(int id)
